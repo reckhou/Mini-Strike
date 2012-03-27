46bfae057c6cbf57c17ee7bc4486696315813e6d
Shuaiying Hou <reckhou@hotmail.com>
2012-03-27 21:49:55 +0800

    ddd

--- Ministrike/Classes/AppDelegate.cpp
+++ Ministrike/Classes/AppDelegate.cpp
@@ -9,6 +9,7 @@
 #include "AppDelegate.h"
 
 #include "cocos2d.h"
+#include "GameMenuScene.h"
 #include "HelloWorldScene.h"
 
 USING_NS_CC;
@@ -90,6 +91,7 @@ bool AppDelegate::applicationDidFinishLaunching()
 	pDirector->setAnimationInterval(1.0 / 60);
 
 	// create a scene. it's an autorelease object
+	//CCScene *pScene = GameMenuScene::scene();
 	CCScene *pScene = HelloWorld::scene();
 
 	// run
